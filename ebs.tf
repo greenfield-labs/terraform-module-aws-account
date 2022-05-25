2437655974fbd61c022f408bb7ea17933ecd20fd
greenfieldz <104225864+greenfieldz@users.noreply.github.com>
2022-05-25 01:33:37 -0500

    feat: allow toggling ebs encryption

--- ebs.tf
+++ ebs.tf
@@ -1,8 +1,9 @@
 # Enables EBS volume encryption at rest by default using the General KMS key
 resource "aws_ebs_default_kms_key" "this" {
+  count   = var.enforce_ebs_encryption ? 1 : 0
   key_arn = aws_kms_key.general.arn
 }
 
 resource "aws_ebs_encryption_by_default" "this" {
-  enabled = true
+  enabled = var.enforce_ebs_encryption
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
 variable "name" {
-    description = "The name of the account"
+  description = "The name of the account"
+}
+
+variable "enforce_ebs_encryption" {
+  description = "Whether to enforce EBS volume encryption at the account level"
+  default     = false
 }
 
 variable "github_actions_oidc_enabled" {
-    default = true
+  default = true
 }
